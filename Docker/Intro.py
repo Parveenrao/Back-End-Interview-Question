0586956d627b682672400f5779c9396b62b363de
Parveenrao <parveeenrao7727@gmail.com>
2026-03-02 22:42:41 +0530

    docker intro

---
+++ Docker/Intro.py
@@ -0,0 +1,19 @@
+""" =>  Docker 
+       
+       1. Docker is a containerization platform 
+       2. It lets you package 
+         
+          a. Your code
+          b. Runtime(Python , Node , java)
+          c. Dependencies 
+          d. System Librarires 
+          e. Os-level configs 
+    
+    Into one portable unit called a container 
+    
+    So it runs same on 
+    1. Your laptop 
+    2. Your friend laptop
+    3. Production server 
+    4. CLoud      
+ """
