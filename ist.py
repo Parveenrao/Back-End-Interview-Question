fe1a4d59cee89a43e89074f5709803b1e3fe9548
Parveenrao <parveeenrao7727@gmail.com>
2026-03-02 14:02:53 +0530

    ist.py

---
+++ ist.py
@@ -0,0 +1,14 @@
+def even(n):
+    if n % 2 == 0:
+        print("Even no")
+        
+    else:
+        print("Odd no")
+
+
+if __name__ == "__main__":
+    
+    hello = even(10)
+    
+    print(even)        
+            
