6a7f4344d842c9ddfb7e93cc5223963fa62eb72a
Parveenrao <parveeenrao7727@gmail.com>
2026-05-04 13:14:35 +0530

    System-Design

---
+++ System_Design/Vertical_Scaling.py
@@ -0,0 +1,49 @@
+""" 
+
+=> Vertical Scaling 
+     
+     -> vertical scaling means (scaling up) making a single machine more powerful  instead of adding more machines
+     
+     -> Same server , bigger machine 
+     
+     
+     -> Examples 
+         
+         4GB Ram -> 32Gb Ram
+         2 core cpu -> 16 core cpu 
+         HDD -> SSD
+    
+    
+    -> Real Example 
+        
+        -> Let say we have food stall 
+        
+        Verical scaling means -> hire more check + bigger stove
+        Horizontal scaling means => opening more stall 
+    
+    
+    -> Limit
+       
+       1. hard Limit
+           
+           -> We cannot scale forever 
+           
+           -> Max Ram exist
+           
+           => Max CPU exist 
+       
+       2. Single Point of failure 
+           
+           One machine = one failure = whole system down 
+       
+       3. Expensive 
+       
+           Biggger machine , more cost
+       
+       4. Down time 
+           
+           -> Often require downtime                      
+
+
+
+"""
