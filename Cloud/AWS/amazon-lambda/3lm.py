aaf777e7b485086d275c080dc01f4f8c2c317dbd
Parveen <parveeenrao7727@gmail.com>
2026-07-07 08:55:40 +0530

    AWS

---
+++ Cloud/AWS/amazon-lambda/3lm.py
@@ -0,0 +1,57 @@
+""" 
+
+=> Iaas (Infrastructure As A Service)
+
+    -> Cloud provider gives you virtual machine
+
+    -> Everything inside the VM is your responsibility
+
+        1. Application 
+        2. Runtime 
+        3. Libraries 
+        4. Operating system 
+        5. Virtual machine 
+        6. Storage 
+        7. Networking 
+        8. Physical Server 
+
+    Think of like renting an empty apartment
+
+
+    -> AWS Example 
+
+       Amazon EC2
+
+       you lanuch an EC instance
+
+
+       1. Install Ubuntu 
+       2. Install python 
+       3. Install Go
+       4. Install Nginx 
+       5. Configure Firewall
+       6. Update packages
+       7. Patch Os
+
+    Everything is your responsibility
+
+=> Paas
+
+    -> Platform as A service
+
+    -> Provider manages 
+
+        1. Servers 
+        2. Operating system 
+        3. Runtime
+
+    we only deploy application
+
+    Think of like renting an fully furnshied application
+
+
+=> Serverless        
+
+
+
+"""
