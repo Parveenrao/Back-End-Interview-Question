3b4daf7c69757d5047e4172483867dcedcb11f89
Parveen <parveeenrao7727@gmail.com>
2026-07-19 22:01:29 +0530

    system

---
+++ System_Design/1Functional-Non-Functional.py
@@ -0,0 +1,162 @@
+""" 
+
+=> What is Functional And Non-Functional Requirements
+
+
+
+=> Requirements
+
+    -> Requirement describe what system do and how well it should do it
+
+
+       Functional       = what 
+       Non-Functional   = how well
+
+       
+    1. Functional Requirements 
+
+       -> Imagine we are designing whatsapp
+
+       -> These are the features user expect 
+
+          1. User registration
+          2. Login 
+          3. Send message 
+          4. Receive message 
+          5. Create Group 
+          6. Delete Group 
+          7. Voice call
+          8. Video call
+          9. Show online status
+
+      -> These define what application should do
+
+
+    2. Non-Functional Requirements
+
+        -> THese describe quality of the system 
+
+            1. Message should be delivered the quality of system
+            2. Support 2 billion users
+            3. 99.999 % availability 
+            4. Data should be encrypted 
+            5. System should survive server failure 
+
+            6. Handle millions of concurrent users
+
+        -> These define how well the application perform              
+
+
+
+
+"""
+
+
+
+""" 
+
+=> Simple Definition
+
+  
+    1. Functional Requirement 
+
+       -> Functional requirement specify the busniess functionality or features the system
+          must provide to users
+
+          Login , Logout ,Search , Upload , Place order , Pay using UPI
+
+
+   2. Non-Functional Requirement 
+
+       -> Non functional requirement specify  the quality , performance , scalability ,
+          security and reliability of the system
+
+
+           Response time , Scalability , Availability , Reliability , Security            
+
+
+
+"""
+
+
+""" 
+
+
+=> Realk Life Example 
+
+   1. Imagine Building a restaurant 
+
+       Functional Requirement
+
+       -> Take orders
+       -> Cook food 
+       -> Deliver food 
+       -> Accept Payment 
+
+
+       Non-Functional Requirement 
+
+        -> Food delivered with in 30 minutes 
+        -> Restaurant open 24 * 7
+
+        -> Can server 500 customers simultaneously 
+
+        -> Hygenic kitchen 
+
+        -> Secure payment  
+
+
+"""
+
+
+""" 
+
+
+=> Interview Question 
+
+
+     1. Desgin Instagram 
+
+        
+       => Functional Requirement 
+
+          1. User signup/login 
+          2. Upload photo/video 
+
+          3. Follow / unfollow users 
+
+          4. Like post 
+
+          5. Comment 
+
+          6. Share post 
+
+          7. News feed 
+
+          8. Search users 
+
+          9. Notifications 
+
+          10. Direct message
+
+       => Non-Functional Requirements 
+
+          1. Feed should load under 200ms 
+
+          2. Images highly available 
+
+          3. Supoort 500 million user daily
+
+          4. Eventuall consistency is acceptable for like 
+
+          5. Securea authentication 
+
+          6. Fault tolerant 
+
+          7. Auto scalabale   
+
+
+
+
+
+"""
