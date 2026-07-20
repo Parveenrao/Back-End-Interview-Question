be1839301f9ef1f213a62818eef889d5fb5002df
Parveen <parveeenrao7727@gmail.com>
2026-07-20 14:33:15 +0530

    API-design

---
+++ fundamentals-backend/api/2api.py
@@ -0,0 +1,61 @@
+""" 
+
+=> Types Of APIs (Application Programming Interface)
+
+    -> An API is a set of rules and protocols that allows different software applications to
+       communicate with each other
+
+
+       A weather app gets weather data from a weather service API
+
+       A payment gateway API processes online payments
+
+
+=> Based On Accessbility
+
+     1. Public APIs (Open APIs)
+ 
+         -> These API are available to external developer and organization
+
+         -> Characteristic
+
+            1. Openly accessible
+            2. Designed for thirdy party developers
+
+     2. Private APIs
+
+        -> Used only withan Organization
+
+        -> Not exposed to external users
+
+        -> improve internal system communication
+
+    3. Partners APIs
+
+       -> Shared only with specific business partners
+
+       -> Controlled access
+
+       -> Require agreement between organizations
+
+
+       -> An airline sharing booking APIs with travel agencies
+
+    4. Composite APIs
+
+       -> Combine multiple API call into a single request
+
+       -> An e-commerce app fetching
+
+          1. customer information 
+
+          2. Order history
+
+          3. payment status
+
+
+          thorugh one API Request              
+
+
+
+"""
