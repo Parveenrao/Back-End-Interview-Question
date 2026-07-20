1eb55c323b302880d258e536723dcf5cad9dfefc
Parveen <parveeenrao7727@gmail.com>
2026-07-20 14:32:33 +0530

    API-design

---
+++ fundamentals-backend/api/1api.py
@@ -0,0 +1,40 @@
+""" 
+
+=> API (Application Programming Interface)
+
+    -> Application = Any software (mobile app ,website , backend service , ML model , database)
+
+    -> Programming = Used through code 
+
+    -> Interface = A contract that allows  two system communicate
+
+
+    -> Program don't directly  access another program internal code , they communicate through api
+
+=> What problem API solve
+
+   -> Imagine there were no APIs
+
+      A frontend developer would need direct database access
+
+        Brower -> database 
+
+     1. security -> anyone could delete data
+
+     2. Different database engine -> frontend must know sql
+
+
+=> API Are Langugae Independent
+
+   1. A client written in javascript can call an api written in python
+
+   2. A Go service can call an API written in RUST
+
+   Becuase they communicate using standard protocols and data formats , the implementations
+
+   langugae does not matter
+
+
+
+
+"""
