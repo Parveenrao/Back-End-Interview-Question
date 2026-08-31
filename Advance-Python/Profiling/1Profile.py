b3fdaa8aeef014fcda57f1a35066a3ab096a4f58
Parveen <parveeenrao7727@gmail.com>
2026-08-31 10:22:52 +0530

    Profiling

---
+++ Advance-Python/Profiling/1Profile.py
@@ -0,0 +1,37 @@
+""" 
+
+=> Profiling 
+
+   -> Profiling is the process of measuring how a program behaves while it is running
+
+   -> it help answer question like 
+
+       1. Which function is taking most time 
+       2. Which line of code is slow 
+       3. How much memory is my application using 
+       4. Which fuction is called most 
+       5. Where are the bottlenecks
+
+    -> Profiling is medical checkup for our application
+
+
+=> What can Profile measure
+
+    1. CPU profile         ->  execution time measure 
+
+    2. Memory profile      ->  Ram usuage
+
+    3. Line profile        ->  Time per line 
+
+    4. Thread profiling    ->  Thread activity
+
+    5. I/O Profiling       ->  Disk and Network operation 
+
+    6. GPU Profiling       ->  GPU computation time 
+
+    7. Database Profiling  ->  SQL query profiling   
+
+
+
+
+"""
